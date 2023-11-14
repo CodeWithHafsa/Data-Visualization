2c26510c1492ae412654b1a85256566b79532807
Hafsa <136093032+CodeWithHafsa@users.noreply.github.com>
2023-11-14 14:06:49 -0800

    Visualization on Wine quality Dataset

---
+++ Day-11 (Wine Quality Dataset)/coord.py
@@ -0,0 +1,9 @@
+
+
+x_coords = {'Alcohol': 0.82, 'Sulphates': 0.77, 'pH': 0.91,
+            'Density': 0.80, 'Total Sulfur Dioxide': 0.59,
+            'Free Sulfur Dioxide': 0.6, 'Chlorides': 0.77,
+            'Residual Sugar': 0.67, 'Citric Acid': 0.76,
+            'Volatile Acidity': 0.67, 'Fixed Acidity': 0.71}
+
+y_coord = 9.8
